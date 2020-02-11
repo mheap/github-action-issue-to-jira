8b77cf282200db6a59d4aa853acfbece43d64019
Michael Heap <m@michaelheap.com>
2020-02-11 18:12:04 +0000

    AddJiraTicket

--- index.js
+++ index.js
@@ -14,11 +14,18 @@ Toolkit.run(async tools => {
     strictSSL: true
   });
 
-  console.log(core.getInput('jiraHost', { required: true }));
-
   const payload = tools.context.payload;
   const title = payload.issue.title;
   const body = payload.issue.body;
 
+  const project = core.getInput('project', { required: true }),
+  const assignee = core.getInput('assignee', { required: true }),
+
+  addJiraTicket(project, title, body, assignee);
+
   tools.exit.success('We did it!')
-})
+});
+
+function addJiraTicket(project, title, body, assignee) {
+  console.log(`Creating ticket in ${project} with ${title}`);
+}

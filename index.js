68f8e72b83d8b47832758162e606ad02e2fd14b6
Michael Heap <m@michaelheap.com>
2020-02-11 18:00:58 +0000

    Add Jira Dependency

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const { Toolkit } = require('actions-toolkit')
+const { Toolkit } = require('actions-toolkit');
+const core = require('@actions/core');
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
 
+  var jira = new JiraApi({
+    protocol: 'https',
+    host: core.getInput('jiraHost', { required: true }),
+    username: core.getInput('jiraUsername', { required: true }),
+    password: core.getInput('jiraPassword', { required: true }),
+    apiVersion: '2',
+    strictSSL: true
+  });
+
+  console.log(core.getInput('jiraHost', { required: true }));
+
   const payload = tools.context.payload;
-  tools.log.info(payload.issue.title);
-  tools.log.info(payload.issue.body);
+  const title = payload.issue.title;
+  const body = payload.issue.body;
 
   tools.exit.success('We did it!')
 })

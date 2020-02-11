056d03091b27da025349facd770db0a1b49863dd
Michael Heap <m@michaelheap.com>
2020-02-11 17:54:15 +0000

    Debug context input

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const { Toolkit } = require('actions-toolkit')
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
+
+  const payload = tools.context.payload;
+  tools.log.info(payload.issue.title);
+  tools.log.info(payload.issue.body);
+
   tools.exit.success('We did it!')
 })

c40a4acd0b8239314ea7d5cfac0d925568e3d8fa
Michael Heap <m@michaelheap.com>
2020-02-18 18:18:24 +0000

    Add the first test

--- index.js
+++ index.js
@@ -2,32 +2,30 @@ const { Toolkit } = require('actions-toolkit');
 const core = require('@actions/core');
 var JiraApi = require('jira-client');
 
-var jira = new JiraApi({
-  protocol: 'https',
-  host: core.getInput('jiraHost', { required: true }),
-  username: core.getInput('jiraUsername', { required: true }),
-  password: core.getInput('jiraPassword', { required: true }),
-  apiVersion: '2',
-  strictSSL: true
-});
-
 // Run your GitHub Action!
 Toolkit.run(async tools => {
+  var jira = new JiraApi({
+    protocol: 'https',
+    host: core.getInput('jiraHost', { required: true }),
+    username: core.getInput('jiraUsername', { required: true }),
+    password: core.getInput('jiraPassword', { required: true }),
+    apiVersion: '2',
+    strictSSL: true
+  });
 
   const payload = tools.context.payload;
   const title = payload.issue.title;
-  console.log(payload);
   const body = `${payload.issue.body}\n\nRaised by: ${payload.issue.user.html_url}\n\n${payload.issue.html_url}`;
 
   const project = core.getInput('project', { required: true });
   const assignee = core.getInput('assignee', { required: true });
 
-  await addJiraTicket(project, title, body, assignee);
+  await addJiraTicket(jira, project, title, body, assignee);
 
   tools.exit.success('We did it!')
 });
 
-function addJiraTicket(project, title, body, assignee) {
+function addJiraTicket(jira, project, title, body, assignee) {
   let request = {
     fields: {
       assignee: {
@@ -43,8 +41,6 @@ function addJiraTicket(project, title, body, assignee) {
       }
     }
   };
-
-  console.log(request);
 
   return jira.addNewIssue(request);
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,6 @@
 const { Toolkit } = require('actions-toolkit')
+const nock = require('nock')
+nock.disableNetConnect()
 
 describe('Issue to Jira', () => {
   let action, tools
@@ -15,8 +17,29 @@ describe('Issue to Jira', () => {
     tools.exit.success = jest.fn()
   })
 
-  it('exits successfully', () => {
-    action(tools)
+  it('exits successfully', async () => {
+    process.env.INPUT_JIRAHOST = "example.com";
+    process.env.INPUT_JIRAUSERNAME = "adminlogin";
+    process.env.INPUT_JIRAPASSWORD = "notmyrealpassword";
+    process.env.INPUT_PROJECT = "SP";
+    process.env.INPUT_ASSIGNEE = "admin";
+
+    tools.context.payload = {
+      "issue": {
+        "title": "Hello World",
+        "body": "This is an example",
+        "html_url": "https://github.com/mheap/action-test/issues/123",
+        "user": {
+            "html_url": "https://github.com/mheap"
+        }
+      }
+    }
+
+    nock('https://example.com')
+      .post('/rest/api/2/issue')
+      .reply(200, {})
+
+    await action(tools)
     expect(tools.exit.success).toHaveBeenCalled()
     expect(tools.exit.success).toHaveBeenCalledWith('We did it!')
   })
